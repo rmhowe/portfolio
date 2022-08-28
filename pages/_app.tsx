c7fb8ba500f7f47e8c6f6eecbe53b523a0d5fd12
Robbie Howe <rmhowe@pm.me>
2022-08-28 14:19:29 +0100

    Adds fathom client setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,28 @@ import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import * as Fathom from 'fathom-client';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load('SCLLPAZS', {
+      includedDomains: ['robbiehowe.xyz'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
