68e550c9a79aa786c2661e37742899a8877ab2ef
Robbie Howe <hello@robbiehowe.xyz>
2025-07-12 21:15:14 +0100

    Lowers default image quality

--- app/utils/cloudflareImageLoader.ts
+++ app/utils/cloudflareImageLoader.ts
@@ -14,10 +14,7 @@ export default function cloudflareLoader({
   if (process.env.NODE_ENV === 'development') {
     return src;
   }
-  const params = [`width=${width}`, 'format=auto'];
-  if (quality) {
-    params.push(`quality=${quality}`);
-  }
+  const params = [`width=${width}`, 'format=auto', `quality=${quality || 70}`];
   const paramsString = params.join(',');
   return `/cdn-cgi/image/${paramsString}/${normalizeSrc(src)}`;
 }
